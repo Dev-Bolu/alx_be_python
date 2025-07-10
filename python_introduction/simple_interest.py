a1a815f83505ec03795814bf1dfe2022e2c25e64
Boluwatife <122864888+Dev-Bolu@users.noreply.github.com>
2025-07-11 00:12:02 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
 Principal = 1000    # Initial investment amount(#1000)
-Rate = 0.05  # Interest rate (5%)
+Rate = 5  # Interest rate (5%)
 Time = 3   # Time period in years (3 years)
 
-Interest = (Principal * Rate * Time)  # Simple interest formula
+Interest = (Principal * Rate * Time) / 100 # Simple interest formula
 
 print("The simple interest is:", Interest)
