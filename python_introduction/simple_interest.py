00355aea4ebb7e3c5fedda591db9c1f29f155d26
Boluwatife <122864888+Dev-Bolu@users.noreply.github.com>
2025-07-11 00:51:19 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,4 +1,8 @@
-(I = P * R * T)
+I = P * R * T
+I  = interest
+P = principal
+T = time
+
 principal = 1000    # Initial investment amount(#1000)
 rate = 0.05  # Interest rate (5%)
 time = 3   # Time period in years (3 years)
