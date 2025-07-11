6642cbe486bc3a1da9090af286a0e2b37ea495fe
Boluwatife <122864888+Dev-Bolu@users.noreply.github.com>
2025-07-11 01:28:37 +0100

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-current_age = int(input("How old are you: "))
+current_age = int(input("How old are you? "))
 #calculate age in the year 2050
 years_to_add = 2050 - 2025
 future_age = current_age + years_to_add
