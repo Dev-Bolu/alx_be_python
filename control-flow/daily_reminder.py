4cd190ff98f3a22d9e8d4358deb3b65729c7a3b5
Bolu <bolu1920@gmail.com>
2025-07-19 11:30:10 +0100

    ....

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -40,23 +40,23 @@ match priority:
 # Provide a customized reminder
 print(f"\n {reminder_message}")
 """
-match Priority:
+match priority:
     case "high":
-        if Time_Bound == "yes":      
-            print(f"Reminder: {Task} is a {Priority} priority that requires immediate attention today!")
+        if time_bound == "yes":      
+            print(f"Reminder: {task} is a {priority} priority that requires immediate attention today!")
         else:
-            print(f"Note: {Task} is marked as {Priority} priority. Consider completing it when you have free time.")
+            print(f"Note: {task} is marked as {priority} priority. Consider completing it when you have free time.")
     case "medium":
-        if Time_Bound == "yes":      
-            print(f"Reminder: {Task} is a {Priority} priority that requires immediate attention today!")
+        if time_bound == "yes":      
+            print(f"Reminder: {task} is a {priority} priority that requires immediate attention today!")
         else:
-            print(f"Note: {Task} is marked as {Priority} priority. Consider completing it when you have free time.")
+            print(f"Note: {task} is marked as {priority} priority. Consider completing it when you have free time.")
             
     case "low":
-        if Time_Bound == "yes":      
-            print(f"Reminder: {Task} is a {Priority} priority that requires immediate attention today!")
+        if time_bound == "yes":      
+            print(f"Reminder: {task} is a {priority} priority that requires immediate attention today!")
         else:
-            print(f"Note: {Task} is marked as {Priority} priority. Consider completing it when you have free time.")
+            print(f"Note: {task} is marked as {priority} priority. Consider completing it when you have free time.")
             
     case _:
         print("Invalid priority level. Please enter high, medium, or low.")
