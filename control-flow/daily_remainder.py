d5ecdaab729bb5780acd1a6fa083df2ab6d1d94e
Bolu <bolu1920@gmail.com>
2025-07-19 01:52:16 +0100

    ...

--- control-flow/daily_remainder.py
+++ control-flow/daily_remainder.py
@@ -3,21 +3,21 @@ Priority = input("Enter the priority (high/medium/low): ").strip().lower()
 Time_bound = input("Is it time-bound, yes/no : ").strip().lower()
 match priority:
     case "high":
-        if time_bound == "yes":      
-            print(f"{task} is marked as {priority} priority that requires immediate attention today!")
+        if Time_bound == "yes":      
+            print(f"{Task} is marked as {Priority} priority that requires immediate attention today!")
         else:
-            print(f"{task} is marked as {priority} priority. Consider completing it when you have free time.")
+            print(f"{Task} is marked as {Priority} priority. Consider completing it when you have free time.")
     case "medium":
-        if time_bound == "yes":      
-            print(f"{task} is marked as {priority} priority that requires immediate attention today!")
+        if Time_bound == "yes":      
+            print(f"{Task} is marked as {Priority} priority that requires immediate attention today!")
         else:
-            print(f"{task} is marked as {priority} priority. Consider completing it when you have free time.")
+            print(f"{Task} is marked as {Priority} priority. Consider completing it when you have free time.")
             
     case "low":
-        if time_bound == "yes":      
-             print(f"{task} is marked as {priority} priority that requires immediate attention today!")
+        if Time_bound == "yes":      
+             print(f"{Task} is marked as {Priority} priority that requires immediate attention today!")
         else:
-            print(f"{task} is marked as {priority} priority. Consider completing it when you have free time.")
+            print(f"{Task} is marked as {Priority} priority. Consider completing it when you have free time.")
             
     case _:
         print("Invalid priority level. Please enter high, medium, or low.")
