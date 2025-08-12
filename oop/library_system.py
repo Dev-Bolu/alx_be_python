e56a71a50389c5a367eceb04c16e61ceb1c9bc6d
Bolu <bolu1920@gmail.com>
2025-08-12 14:57:03 +0100

    ...

--- oop/library_system.py
+++ oop/library_system.py
@@ -24,7 +24,7 @@ class PrintBook(Book):
         self.page_count = page_count
      
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count} "
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
     
     def __repr__(self):
         return f"PrintBook: ('{self.title}', '{self.author}', Page Count: {self.page_count})"
